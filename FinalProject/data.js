210afffcd39a973cb2fc7707abd8abc0b006fa79
haypete17 <60486664+haypete17@users.noreply.github.com>
2020-04-29 16:10:01 -0500

    Update and rename data.geojson to data.js

--- FinalProject/data.js
+++ FinalProject/data.js
@@ -1,8 +1,6 @@
+var data =
 {"type":"FeatureCollection","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[
 {
-   "type": "FeatureCollection",
-   "features": [
-  {
     "type": "Feature",
     "geometry": {
        "type": "Point",
