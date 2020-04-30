7ff624167fb92c1642621a76c16251c120892ae8
haypete17 <60486664+haypete17@users.noreply.github.com>
2020-04-30 00:55:12 -0500

    Update restaurantfinal.js

--- FinalProject/restaurantfinal.js
+++ FinalProject/restaurantfinal.js
@@ -1,8 +1,5 @@
 var datatwo=
 {"type":"FeatureCollection","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[
-{
-   "type": "FeatureCollection",
-   "features": [
   {
     "type": "Feature",
     "geometry": {
